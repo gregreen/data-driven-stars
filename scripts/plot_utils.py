2c0c7831d299257d94ca4206f4667c56b80fd996
Gregory M. Green <gregorymgreen@gmail.com>
2020-01-27 18:55:51 +0100

    Plot trends in residuals, chisq/dof

---
+++ scripts/plot_utils.py
@@ -0,0 +1,326 @@
+#!/usr/bin/env python
+#
+# plot_utils.py
+# Useful plotting functions.
+#
+# Copyright (C) 2016  Gregory M. Green
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
+from __future__ import print_function
+
+import numpy as np
+import matplotlib.ticker as ticker
+
+
+def unit_formatter(base_unit, fmt='{:g} {:s}', prefixes=None, sigfigs=None):
+    if prefixes is None:
+        prefixes = ['', 'k', 'M', 'G', 'T']
+    
+    def f(x):
+        order = int(np.floor(np.log10(np.abs(x)) / 3))
+        z = x * 10.**(-3.*order)
+        if sigfigs is not None:
+            z = float(('{:.'+str(sigfigs)+'g}').format(z))
+        return fmt.format(z, prefixes[order] + base_unit)
+    
+    return f
+
+
+def rel2abs_ax_pos(ax, x, y):
+    lim = (ax.get_xlim(), ax.get_ylim())
+    return (l[0] + z * (l[1]-l[0]) for z,l in zip((x,y),lim))
+
+
+def pm_f_formatter(n_digits=2):
+    fmt_str = r'${{:+.{:d}f}}$'.format(n_digits)
+    zero_str = r'$+0.' + n_digits * '0' + r'$'
+    zero_str_correct = r'$\pm 0.' + n_digits * '0' + r'$'
+
+    def x2str(x, pos):
+        txt = fmt_str.format(x)
+        if txt == zero_str:
+            return zero_str_correct
+        return txt
+
+    return ticker.FuncFormatter(x2str)
+
+
+def pm_deg_formatter(pad_negative=False):
+    """
+    Returns a function that formats angles with a "+-"-sign and a degree
+    symbol.
+    """
+    fmt_str = r'${:+g}^{{\circ}}$'
+    zero_str = r'$+0^{\circ}$'
+    zero_str_correct = r'$\pm 0^{\circ}$'
+
+    def f(theta):
+        txt = fmt_str.format(theta)
+        if txt == zero_str:
+            return zero_str_correct
+        if pad_negative and txt.startswith(r'$-'):
+            txt = txt[:-1] + r'\ $'
+        return txt
+
+    return f
+
+
+def percentile_weighted(x, w, pct):
+    idx = np.argsort(x)
+    w_cumulative = 100. * np.cumsum(w[idx_sort]) / np.sum(w)
+    idx = np.searchsorted(w_cumulative, pct)
+    idx = np.clip(idx, 0, x.size-1)
+    return x[idx]
+
+
+def correlation_plot(ax, x, y,
+                     x_bins=25,
+                     y_bins=20,
+                     ymax_pct=99.,
+                     ymax_abs=None,
+                     weights=None,
+                     norm='equal_ink',
+                     pct=[15.87, 50., 84.13],
+                     ax_hist=None,
+                     fontsize=10.):
+    """
+    Generates a Hogg-style correlation plot between variables ``x`` and ``y`` on
+    the given axes.
+
+    Args:
+        ax (``matplotlib.axes._axes.Axes``): Axes on which to draw correlation
+            plot.
+        x (``numpy.ndarray``): Values to put on the x-axis.
+        y (``numpy.ndarray``): Values to put on the y-axis. Typically the
+            difference between two variables to be compared.
+        x_bins (Optional[int or float array]): Number of bins along the x-axis.
+            Defaults to ``25``. If an array is provided, then it is interpreted
+            as the bin edges.
+        y_bins (Optional[int or float array]): Number of bins along the y-axis.
+            Defaults to ``20``. If an array is provided, then it is interpreted
+            as the bin edges. If ``y_bins`` is an array, it overrides both
+            ``ymax_pct`` and ``ymax_abs``.
+        ymax_pct (Optional[float]): The y-axis will extend to this percentile of
+            the absolute values of ``y``. Defaults to ``99``.
+        ymax_abs (Optional[float]): The y-axis limits will be set to this value.
+            If ``None`` (the default), the ``ymax_pct`` will be used instead to
+            set the y-axis limits.
+        weights (Optional[float array]): Statistical weight of each value in the
+            ``x`` and ``y`` arrays. Defaults to ``None`` (i.e., equal weights).
+        norm (Optional[str]): The method to use to normalize the densities in
+            the correlation plot. There are three possible values:
+            ``'equal_ink'`` (the default) uses an equal amount of ink for each
+            x-bin; ``'max'`` normalizes each x-bin separately to the maximum
+            value in the bin; ``'total_weight'`` simply represents the total
+            statistical weight in each xy-bin.
+        pct (Optional[length-3 array]): The percentiles to show on the
+            correlation plot. Defaults to ``[15.87, 50., 84.13]``, corresponding
+            to the median and 1-sigma-equivalent percentiles (for a Gaussian).
+        ax_hist (Optional[``matplotlib.axes._axes.Axes``]): Axes on which to
+            plot a histogram of ``y``. Ideally, this axis should be to the right
+            or left of ``ax``. Defaults to ``None``, meaning that no histogram
+            is plotted.
+        fontsize (Optional[float]): Base fontsize of labels. Some labels (e.g.,
+            histogram labels) will have slightly smaller fontsizes. Defaults to
+            10.
+
+    Raises:
+        ``ValueError``: ``pct`` or ``norm`` are not correctly specified.
+    """
+
+    # Determine limits on y
+    if ymax_abs is None:
+        y_max = 1.1 * np.percentile(np.abs(y), ymax_pct)
+    else:
+        y_max = ymax_abs
+
+    # x- and y-bin edges
+    if not hasattr(x_bins, '__len__'):
+        x_bins = np.linspace(np.min(x), np.max(x), x_bins+1)
+
+    if not hasattr(y_bins, '__len__'):
+        y_bins = np.linspace(-y_max, y_max, y_bins+1)
+
+    n_bins = (len(x_bins)-1, len(y_bins)-1)
+
+    # Set up empty density map and percentile thresholds
+    density = np.zeros(n_bins, dtype='f4')
+    thresholds = np.zeros((n_bins[0], 3), dtype='f4')
+
+    # Percentiles to mark
+    if not hasattr(pct, '__len__'):
+        raise ValueError('`pct` must have length 3.')
+        if len(pct) != 3:
+            raise ValueError('`pct` must have length 3.')
+
+    # Density histogram and percentile thresholds in each x bin
+    for n,(x0,x1) in enumerate(zip(x_bins[:-1], x_bins[1:])):
+        idx = (x >= x0) & (x < x1)
+
+        if np.any(idx):
+            y_sel = y[idx]
+
+            if weights is None:
+                thresholds[n,:] = np.percentile(y_sel, pct)
+                w = None
+            else:
+                w = weights[idx]
+                thresholds[n,:] = np.percentile(y_sel, w, pct)
+
+            density[n,:], _ = np.histogram(
+                y_sel,
+                bins=y_bins,
+                density=False,
+                range=[-y_max, y_max],
+                weights=w)
+
+    # Normalize density map
+    if norm == 'max':
+        a = np.max(density, axis=1)
+        a[a == 0] = 1
+        density /= a[:,None]
+        #density[~np.isfinite(density)] = 0.
+    elif norm == 'equal_ink':
+        a = np.sum(density, axis=1)
+        a[a == 0] = 1
+        density /= a[:,None]
+        # density[~np.isfinite(density)] = 0.
+        norm = np.percentile(np.max(density, axis=1), 90.)
+        print(np.max(density, axis=1))
+        print(norm)
+        if norm == 0:
+            norm = np.max(density)
+        norm = np.clip(norm, 0., 0.25)
+        print(norm)
+        print('')
+        density /= norm
+    elif norm == 'total_weight':
+        density /= np.max(density)
+    else:
+        raise ValueError("`norm` must be 'max', 'equal_ink' or 'total_weight'.")
+
+    # Fix NaN densities
+    idx = ~np.isfinite(density)
+    density[idx] = 0.
+
+    # Plot density in background
+    extent = (x_bins[0], x_bins[-1], y_bins[0], y_bins[-1])
+
+    ax.imshow(
+        density.T,
+        extent=extent,
+        origin='lower',
+        aspect='auto',
+        cmap='gray_r',
+        interpolation='nearest',
+        vmin=0.,
+        vmax=1.)
+
+    # Line at y = 0
+    ax.axhline(y=0., c='c', ls='-', alpha=0.3)
+
+    # Histogram of y-values
+    if ax_hist is not None:
+        # ax_hist.hist(
+        #     y,
+        #     bins=y_bins,
+        #     weights=weights,
+        #     orientation='horizontal',
+        #     color='k',
+        #     alpha=0.25)
+
+        bin_val, bin_edges = np.histogram(y, bins=y_bins, weights=weights)
+        # ax_hist.barh(bin_edges, bin_val, align='edge')
+
+        if weights is None:
+            y_pct = np.percentile(y, pct)
+        else:
+            y_pct = percentile_weighted(y, weights, pct)
+
+        y_pct_padded = np.hstack([-np.inf, y_pct, np.inf])
+        hist_alpha = [0.25, 0.5, 0.5, 0.25]
+
+        for k, (y0, y1) in enumerate(zip(y_pct_padded[:-1], y_pct_padded[1:])):
+            idx0, idx1 = np.searchsorted(bin_edges, [y0, y1])
+
+            section_edges = np.hstack([y0, bin_edges[idx0:idx1], y1])
+
+            w_idx0, w_idx1 = idx0-1, idx1
+
+            if section_edges[0] < bin_edges[0]:
+                section_edges = section_edges[1:]
+                w_idx0 += 1
+            if section_edges[-1] > bin_edges[-1]:
+                section_edges = section_edges[:-1]
+                w_idx1 -= 1
+
+            w_section = bin_val[w_idx0:w_idx1]
+
+            # print('(y0, y1) = ({:.2f}, {:.2f})'.format(y0, y1))
+            # print('(i0, i1) = ({:d}, {:d})'.format(idx0, idx1))
+            # print('(w0, w1) = ({:d}, {:d})'.format(w_idx0, w_idx1))
+
+            # idx_bin_keep = (bin_edges > y0) & (bin_edges < y1)
+            # section_edges = hp.hstack([bin_edges])
+
+            # section_edges = bin_edges[:]
+            # w_section = bin_val
+            y_section = 0.5 * (section_edges[1:] + section_edges[:-1])
+
+            ax_hist.hist(
+                y_section,
+                bins=section_edges,
+                weights=w_section,
+                orientation='horizontal',
+                histtype='stepfilled',
+                color='k',
+                edgecolor='k',
+                alpha=hist_alpha[k])
+
+        # for yy in y_pct:
+        #     ax_hist.axhline(y=yy, c='b', ls='-', alpha=0.5)
+
+        ax_hist.yaxis.tick_right()
+        ax_hist.tick_params(axis='y', which='both', direction='out')
+        # ax_hist.yaxis.set_major_formatter(pm_f_formatter(n_digits=2))
+        # ax_hist.yaxis.set_ticks([])
+        ax_hist.yaxis.set_minor_locator(ticker.AutoMinorLocator())
+        ax_hist.yaxis.set_ticklabels([])
+        # ticker.FormatStrFormatter('$%.3f$'))
+
+        ax_hist.set_ylim(extent[2:])
+        ax_hist.set_xticks([])
+        ax_hist.axis('off')
+        # ax_hist.set_yticks(y_pct)
+        # ax_hist.tick_params(axis='y', labelsize=0.8*fontsize)
+
+    # Envelope of percentile thresholds
+    x_range = np.linspace(x_bins[0], x_bins[-1], n_bins[0]+1)
+    for i in range(3):
+        y_envelope = np.hstack([[thresholds[0,i]], thresholds[:,i]])
+        ax.step(x_range, y_envelope, where='pre', c='b', alpha=0.5, lw=1.0)
+
+    # Set axis limits
+    ax.set_xlim(extent[:2])
+    ax.set_ylim(extent[2:])
+
+    # Set axis ticks
+    ax.xaxis.set_minor_locator(ticker.AutoMinorLocator())
+    ax.yaxis.set_minor_locator(ticker.AutoMinorLocator())
+    ax.tick_params(axis='both', labelsize=fontsize)
+    
+    ax.tick_params(axis='y', right=True, labelright=False)
---
+++ scripts/prediction_trends.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python
+
+from __future__ import print_function, division
+
+import numpy as np
+import h5py
+import scipy.stats
+
+import matplotlib.pyplot as plt
+from matplotlib.ticker import AutoMinorLocator
+
+from plot_utils import correlation_plot
+
+
+fmt = 'png'
+
+
+def load_predictions(fname):
+    """
+    Loads neural network predictions:
+        data       - Input data
+        y_obs      - Observed (G, X1-G, X2-G, ...)
+        y_pred     - Predicted (G, X1-G, X2-G, ...)
+        cov_y      - Covariance for each observation
+        reddening  - Inferred reddening of each star
+        R          - Inferred reddening vector
+    Returns a dictionary with each of the above elements.
+    """
+    d = {}
+
+    # Load all fields stored in file
+    with h5py.File(fname, 'r') as f:
+        for key in f.keys():
+            d[key] = f[key][:]
+        d['R'] = f.attrs['R'][:]
+
+    # Calculate reddened predictions
+    d['y_pred_red'] = d['y_pred'] + d['reddening'][:,None]*d['R'][None,:]
+
+    # Calculate chi^2/d.o.f.
+    dy = d['y_pred_red'] - d['y_obs']
+
+    icov = np.empty_like(d['cov_y'])
+    for k,c in enumerate(d['cov_y']):
+        icov[k] = np.linalg.inv(c)
+
+    #nu = dy.shape[1] # Number of parameters << number of observations
+    #print(f'nu = {nu}')
+    obs = np.empty(dy.shape, dtype='bool')
+    for k in range(dy.shape[1]):
+        obs[:,k] = (d['cov_y'][:,k,k] < 100.)
+    d['nu'] = np.count_nonzero(obs, axis=1)
+    print(d['nu'])
+    print(np.median(d['nu']))
+    print(np.mean(d['nu']))
+
+    d['rchi2'] = np.einsum('ni,nij,nj->n', dy, icov, dy) / d['nu']
+
+    return d
+
+
+def rchi2_hist(pred):
+    fig = plt.figure(figsize=(8,6), dpi=150)
+    ax = fig.add_subplot(1,1,1)
+
+    bins = np.arange(0., 5.01, 0.1)
+    ax.hist(pred['rchi2'], bins=bins, density=True)
+
+    ylim = ax.get_ylim()
+
+    r = np.linspace(0., bins[-1], 1000)
+    nu = pred['y_obs'].shape[1]
+
+    for nu_k in range(1,nu+1):
+        pdf = scipy.stats.chi2(nu_k).pdf(r*nu_k)*nu_k#/(bins[1]-bins[0])
+        ax.plot(r, pdf, c='b', alpha=0.25)
+
+    ax.set_xlim(0., bins[-1])
+    ax.set_ylim(ylim)
+    ax.xaxis.set_minor_locator(AutoMinorLocator())
+
+    fig.savefig(f'plots/rchi2_hist.{fmt}', dpi=150)
+    plt.close(fig)
+
+
+def rchi2_trends(pred):
+    y = pred['rchi2']
+    y_bins = np.arange(0., 15.01, 0.25)
+
+    x_fields = [
+        ('teff', pred['data']['teff'], r'$T_{\mathrm{eff}}$', (3500.,8500.)),
+        ('logg', pred['data']['logg'], r'$\log \left( g \right)$', (0.6,5.4)),
+        ('feh', pred['data']['feh'], r'$\left[ \mathrm{Fe} / \mathrm{H} \right]$', (-2.5,0.5)),
+        ('reddening', pred['reddening'], r'$\mathrm{reddening}$', (0.,1.25))
+    ]
+
+    for key,x,label,lim in x_fields:
+        fig = plt.figure(figsize=(8,6), dpi=150)
+        ax = fig.add_subplot(1,1,1)
+        
+        idx = np.isfinite(x) & np.isfinite(y) & (x > lim[0]) & (x < lim[1])
+        correlation_plot(ax, x[idx], y[idx], y_bins=y_bins)#, norm='max')
+        
+        ax.set_xlabel(label)
+        ax.set_ylabel(r'$\chi^2 / \nu$')
+        
+        fig.savefig(f'plots/rchi2_trend_{key}.{fmt}')
+        plt.close(fig)
+
+
+def residual_trends(pred):
+    # Calculate residuals in (G, G-BP, BP-RP, RP-g, g-r, ...)-space
+    dy = pred['y_obs'] - pred['y_pred_red']
+    dy[:,1:] = dy[:,:-1] - dy[:,1:]
+
+    obs = np.empty(dy.shape, dtype='bool')
+    obs[:,0] = (pred['cov_y'][:,0,0] < 100.)
+    dy_over_err = np.empty_like(dy)
+    dy_over_err[:,0] = dy[:,0] / np.sqrt(pred['cov_y'][:,0,0])
+    for k in range(1,dy.shape[1]):
+        c = (
+              pred['cov_y'][:,k,k]
+            + pred['cov_y'][:,k-1,k-1]
+            - 2. * pred['cov_y'][:,k,k-1]
+        )
+        #c -= 2. * 0.02**2
+        obs[:,k] = (
+              (pred['cov_y'][:,k,k] < 100.)
+            & (pred['cov_y'][:,k-1,k-1] < 100.)
+        )
+        dy_over_err[:,k] = dy[:,k] / np.sqrt(c)
+
+    x_fields = [
+        ('teff', pred['data']['teff'], r'$T_{\mathrm{eff}}$', (3500.,8500.)),
+        ('logg', pred['data']['logg'], r'$\log \left( g \right)$', (0.6,5.4)),
+        ('feh', pred['data']['feh'], r'$\left[ \mathrm{Fe} / \mathrm{H} \right]$', (-2.5,0.5)),
+        ('reddening', pred['reddening'], r'$\mathrm{reddening}$', (0.,1.25))
+    ]
+
+    y_bins = np.arange(-4., 4.01, 0.2)
+
+    bands = ['G', 'BP', 'RP'] + list('grizyJH') + ['K_s','W_1','W_2']
+    y_cols = (
+          [bands[0]]
+        + [f'{b1}-{b2}' for b1,b2 in zip(bands[:-1],bands[1:])]
+    )
+
+    for k,col_label in enumerate(y_cols):
+        col_label_simple = col_label.replace('-','').replace('_','')
+        if k != 0:
+            col_label = r'\left( {} \right)'.format(col_label)
+
+        y = dy_over_err[:,k]
+
+        for key,x,xlabel,lim in x_fields:
+            fig = plt.figure(figsize=(8,6), dpi=150)
+            ax = fig.add_subplot(1,1,1)
+            
+            idx = (
+                  np.isfinite(x) & np.isfinite(y) & obs[:,k]
+                & (x > lim[0]) & (x < lim[1])
+            )
+            correlation_plot(ax, x[idx], y[idx], y_bins=y_bins)
+            
+            ax.set_xlabel(xlabel)
+
+            ylabel = ' - '.join([
+                r'{}_{{ \mathrm{{ {} }} }}'.format(col_label,sub)
+                for sub in ('obs', 'pred')
+            ])
+            ylabel = r'$\left[ {} \right] / \sigma$'.format(ylabel)
+            ax.set_ylabel(ylabel)
+            
+            fig.savefig(f'plots/d{col_label_simple}_trend_{key}.{fmt}')
+            plt.close(fig)
+
+
+def main():
+    fname = 'data/predictions_dmfix2_2hidden_it29.h5'
+    pred = load_predictions(fname)
+
+    print('chi^2/nu = {}'.format(np.mean(pred['rchi2'])))
+
+    rchi2_hist(pred)
+    rchi2_trends(pred)
+    residual_trends(pred)
+
+    return 0
+
+if __name__ == '__main__':
+    main()
+
